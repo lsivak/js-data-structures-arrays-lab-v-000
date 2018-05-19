3a4903756a5adabf62aae9614f5bb4a9f63ba555
Lara Sanders <larasanders@gmail.com>
2018-05-19 18:10:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const drivers = ["Milo", "Otis", "Garfield"];
 destructivelyAppendDriver() {
-  destructivelyAppendDriver = drivers.push(name)
+  destructivelyAppendDriver(name) = drivers.push(name)
 }
